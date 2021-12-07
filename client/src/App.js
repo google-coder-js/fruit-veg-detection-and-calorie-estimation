09a07395133bb41a85e49eb4c0be180e9cbc34ba
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2021-12-07 23:39:36 +0530

    Add image upload and preview feat

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MainView from './components/MainView/MainView';
 
 function App() {
   return (
     <div className="App">
       Fruit Vegetable detection and Calorie estimation
+       <MainView/>
     </div>
   );
 }
---
+++ client/src/components/MainView/MainView.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios';
+import './mainView.css';
+
+const Upload = () => {
+    const CUSTOM_VISION_API = process.env.REACT_APP_CUSTOM_VISION_API
+    const CUSTOM_VISION_PREDICTION_KEY = process.env.REACT_APP_CUSTOM_VISION_PREDICTION_KEY 
+    const [image, setImage] = useState("")
+    const [predictionData, setPredictionData] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+
+   const canvasRef = useRef(null);
+   const imgRef = useRef(null);
+
+    useEffect( async() => {
+        if (image) {
+             
+            const url = await URL.createObjectURL(image);
+            console.log("url", url);
+            setImageUrl(url);           
+        }  
+            return () => {
+            URL.revokeObjectURL(image)
+        }
+    }, [image])
+
+
+    const uploadFile = async (e) => {
+        e.preventDefault();
+        console.log('image',image);
+        const url = await URL.createObjectURL(image)
+        console.log("url", url);      
+        setImageUrl(url);
+    
+        try {
+            console.log('i',image);
+            const res = await axios.post(
+              CUSTOM_VISION_API,  
+              image,
+                {
+                    headers: {
+                        "content-type": "application/octet-stream",
+                        "Prediction-Key": CUSTOM_VISION_PREDICTION_KEY
+                    },
+                }
+                );
+                console.log(res);
+            setPredictionData(res.data.predictionData);
+        } catch (err) {
+           console.log(err);
+         }
+    }
+    return (
+      <div>
+        <form onSubmit={uploadFile}>
+          <input
+            className="select-file"
+            type="file"
+            name="file upload"
+            id="fileupload"
+            onChange={(e) => setImage(e.target.files[0])}
+            accept="image/*"
+          />
+          <button className="submit-image" type="submit">
+            submit
+          </button>
+        </form>
+        <img useRef={imgRef} className="image-preview" src={imageUrl} alt="" srcset="" />
+        <br/>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+          }}
+        />
+      </div>
+    );
+}
+
+export default Upload;
